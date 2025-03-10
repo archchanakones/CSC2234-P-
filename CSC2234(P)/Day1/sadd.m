6ee8f4247210812b23d8092c09cbb0e224c39619
archchanakones <archchanakones@gmail.com>
2025-03-10 13:21:04 +0530

    Add files via upload

---
+++ CSC2234(P)/Day1/sadd.m
@@ -0,0 +1,17 @@
+
+
+A = [1, 2, 3; 4, 5, 6; 7, 8, 9];  
+B = [9, 8, 7; 6, 5, 4; 3, 2, 1]; 
+
+
+C = A + B;
+
+
+disp('Matrix A:');
+disp(A);
+
+disp('Matrix B:');
+disp(B);
+
+disp('Sum of Matrix A and B (Matrix C):');
+disp(C);
